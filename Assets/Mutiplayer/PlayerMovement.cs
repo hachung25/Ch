78ef908e238dc953c6e32c151811a1340c737565
Minh Nguyễn <nguyendmph48948@gmail.com>
2025-07-15 21:52:49 +0700

    up

--- Assets/Mutiplayer/PlayerMovement.cs
+++ Assets/Mutiplayer/PlayerMovement.cs
@@ -3,22 +3,14 @@ using Fusion;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    public float speed = 5f;
-    private Rigidbody2D rb;
-
-    public override void Spawned()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    public float moveSpeed = 5f;
 
     public override void FixedUpdateNetwork()
     {
-        if (!HasInputAuthority) return;
-
-        float horizontal = Input.GetAxis("Horizontal");
-        Vector2 velocity = rb.linearVelocity;
-        velocity.x = horizontal * speed;
-        rb.linearVelocity = velocity;
+        if (Object.HasInputAuthority)
+        {
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+            transform.Translate(input * moveSpeed * Runner.DeltaTime);
+        }
     }
-
 }
