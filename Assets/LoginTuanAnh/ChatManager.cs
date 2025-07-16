b407155d01320ddb32c2f0b37ea534e0cd2f2d0a
Tuynh-Ozt <Co.gokuz123@gmail.com>
2025-07-16 09:42:24 +0700

    ChatOnline

---
+++ Assets/LoginTuanAnh/ChatManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Fusion; // Cần thiết cho NetworkBehaviour và RPC
+using TMPro;
+
+public class ChatManager : NetworkBehaviour
+{
+    public static ChatManager Instance;
+    private List<string> chatMessages = new List<string>();
+    public ChatUI chatUI;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RpcReceiveChatMessage(string playerName, string message)
+    {
+        string formattedMessage = $"{playerName}: {message}";
+        chatMessages.Add(formattedMessage);
+        chatUI.chatContent.text += formattedMessage + "\n";
+    }
+
+    public void SendChatMessage(string message)
+    {
+        string playerName = Runner.LocalPlayer.PlayerId.ToString();
+        RpcReceiveChatMessage(playerName, message);
+    }
+}
---
+++ Assets/LoginTuanAnh/ChatUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Fusion;
+
+public class ChatUI : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public Button sendButton;
+    public TextMeshProUGUI chatContent;
+
+    private void Start()
+    {
+        sendButton.onClick.AddListener(SendMessage);
+    }
+
+    private void SendMessage()
+    {
+        string message = inputField.text;
+        if (!string.IsNullOrEmpty(message))
+        {
+            ChatManager.Instance.SendChatMessage(message);
+            inputField.text = ""; // Xóa nội dung sau khi gửi
+        }
+    }
+}
