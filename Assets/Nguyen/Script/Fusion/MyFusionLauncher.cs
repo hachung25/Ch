af499d517f59b72adea0cb39365810da58fc2134
Minh Nguyễn <nguyendmph48948@gmail.com>
2025-07-30 22:38:52 +0700

    button start shared mode

---
+++ Assets/Nguyen/Script/Fusion/MyFusionLauncher.cs
@@ -0,0 +1,31 @@
+﻿using Fusion;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyFusionLauncher : MonoBehaviour
+{
+    public NetworkRunner runnerPrefab;
+    public Button playButton;
+
+    void Start()
+    {
+        if (playButton != null)
+            playButton.gameObject.SetActive(true); // Bật nút khi game bắt đầu
+    }
+
+    public void StartSharedClient()
+    {
+        if (playButton != null)
+            playButton.gameObject.SetActive(false); // Ẩn nút sau khi bấm
+
+        var runner = Instantiate(runnerPrefab);
+        runner.ProvideInput = true;
+
+        runner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Shared,
+            SessionName = "DefaultRoom",
+            SceneManager = runner.GetComponent<NetworkSceneManagerDefault>()
+        });
+    }
+}
