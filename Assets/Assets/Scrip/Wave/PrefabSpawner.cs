9786bd1d60503ee218ddd87f8147ca3a6e550dda
hachung <chunghtph48886@gmail.com>
2025-08-27 22:53:12 +0700

    Fixt

--- Assets/Assets/Scrip/Wave/PrefabSpawner.cs
+++ Assets/Assets/Scrip/Wave/PrefabSpawner.cs
@@ -3,13 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MapSpawner : MonoBehaviour
 {
+    public GameObject panelMode;
+
+
+    public void ShowPanelMode()
+    {
+        panelMode.SetActive(true);
+    }
+    
     public void loadScenemap()
     {
         SceneManager.LoadScene("Mode1");
+        panelMode.SetActive(false);
     } 
     public void loadScenemap2()
     {
         SceneManager.LoadScene("Mode2");
+        panelMode.SetActive(false);
     }
     
 }
+
